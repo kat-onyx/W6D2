9139a464d1307b7093317f23876bbdf296481de0
kat-onyx <sarah.kathryn.peters@gmail.com>
2018-11-27 15:01:02 -0800

    Complete setupBoard, makeMove, and bindEvents for tic-tac-toe

--- tic-tac-toe/src/ttt-view.js
+++ tic-tac-toe/src/ttt-view.js
@@ -3,19 +3,47 @@ class View {
     this.game = game;
     this.$el = $el;
     this.setupBoard();
+    this.bindEvents();
   }
 
-  bindEvents() {}
+  bindEvents() {
+    const $ul = $('ul');
+    
+    $ul.on('click', '.square', (e) => {
+      let target = $(e.target);
+      console.log(target.data('pos'));
+      debugger;
+      if (this.game.board.isEmptyPos(target.data('pos'))) {
+        this.makeMove(target);
+        this.game.playMove(target.data('pos'));
+      } else {
+        alert('Invalid move');
+      }
+    });
+  }
 
-  makeMove($square) {}
+  makeMove($square) {
+    // this.game.playMove($s.data('pos'));
+    $square.text(this.game.currentPlayer);
+  }
 
   setupBoard() {
+    const pos = [];
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        pos.push([i, j]);
+      }
+    }
+    
     this.$el.html("<ul></ul>");
     const $ul = $('ul');
-    for (let i = 0; i < 9; i++) {
+    for(let k = 0; k < 9; k++) {
       $ul.append("<li></li>");
-      $('li').attr('id','square');
+      let $li = $('li');
+      $('li').addClass('square');
+      $li.eq(k).data('pos', pos[k]);
     }
+  
     $ul.addClass('grid');
   }
 }
